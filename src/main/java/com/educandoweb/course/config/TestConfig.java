b847765eb808793897e07c4ae68618c2a34ff0be
Davi Silva <davisilvaphoto@gmail.com>
2020-11-10 19:49:33 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -22,7 +22,7 @@ public class TestConfig implements CommandLineRunner{
 	 User u1 = new User(null, "Maria Brown", "maria@gmail.com", "88 8888 7777", "123456");
 	 User u2 = new User(null, "Alex Green", "alex@gmail.com", "88 8888 7777", "123456");
 	 
-	 userRepository.saveAll(Arrays.asList(u1,u2) );
+	 userRepository.saveAll(Arrays.asList(u1,u2));
     }
     
     
