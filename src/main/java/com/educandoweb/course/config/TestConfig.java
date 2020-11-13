aeb9598514bb3292ed74003c07b345f1e4ac154c
Davi Silva <davisilvaphoto@gmail.com>
2020-11-13 18:37:30 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.Payment;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -64,14 +65,14 @@ public class TestConfig implements CommandLineRunner {
 
 	productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-	User u1 = new User(null, "Maria Brown", "maria@gmail.com", "88 8888 7777", "123456");
-	User u2 = new User(null, "Alex Green", "alex@gmail.com", "88 8888 7777", "123456");
+	User user1 = new User(null, "Maria Brown", "maria@gmail.com", "88 8888 7777", "123456");
+	User user2 = new User(null, "Alex Green", "alex@gmail.com", "88 8888 7777", "123456");
 
-	Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-	Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-	Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+	Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+	Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+	Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
-	userRepository.saveAll(Arrays.asList(u1, u2));
+	userRepository.saveAll(Arrays.asList(user1, user2));
 	orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
 	OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
@@ -80,6 +81,18 @@ public class TestConfig implements CommandLineRunner {
 	OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 	orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+	Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+	o1.setPayment(pay1);
+	
+	orderRepository.save(o1);
+	
+	
+	
+	
+	
+	
+	
 
     }
 
