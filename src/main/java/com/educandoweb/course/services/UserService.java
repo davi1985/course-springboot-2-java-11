4f4560f73854a3a7834b87b0960d14126c2e8ebb
Davi Silva <davisilvaphoto@gmail.com>
2020-11-14 07:36:38 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -21,8 +21,11 @@ public class UserService {
 
     public User findById(Long id) {
 	Optional<User> user = repository.findById(id);
-
 	return user.get();
+    }
+
+    public User insert(User obj) {
+	return repository.save(obj);
     }
 
 }
