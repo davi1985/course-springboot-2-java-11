2441c804df1ec04e83410ecdefd2720e63ff8042
Davi Silva <davisilvaphoto@gmail.com>
2020-11-14 07:51:06 -0300

    User delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -12,20 +12,25 @@ import com.educandoweb.course.repositories.UserRepository;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repository;
+	@Autowired
+	private UserRepository repository;
 
-    public List<User> findAll() {
-	return repository.findAll();
-    }
+	public List<User> findAll() {
+		return repository.findAll();
+	}
 
-    public User findById(Long id) {
-	Optional<User> user = repository.findById(id);
-	return user.get();
-    }
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.get();
+	}
 
-    public User insert(User obj) {
-	return repository.save(obj);
-    }
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
 }
