b617a4d0a9837fcf89470e930e41e831b8989159
Davi Silva <davisilvaphoto@gmail.com>
2020-11-14 08:29:19 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -12,25 +12,37 @@ import com.educandoweb.course.repositories.UserRepository;
 @Service
 public class UserService {
 
-	@Autowired
-	private UserRepository repository;
+    @Autowired
+    private UserRepository repository;
 
-	public List<User> findAll() {
-		return repository.findAll();
-	}
+    public List<User> findAll() {
+        return repository.findAll();
+    }
 
-	public User findById(Long id) {
-		Optional<User> user = repository.findById(id);
-		
-		return user.get();
-	}
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
 
-	public User insert(User obj) {
-		return repository.save(obj);
-	}
+        return user.get();
+    }
 
-	public void delete(Long id) {
-		repository.deleteById(id);
-	}
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
 
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getOne(id);
+        updateData(entity, obj);
+
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
